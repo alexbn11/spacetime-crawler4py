65be7d8993023de487431af15c766d3387640fe2
alexbn11 <62927559+alexbn11@users.noreply.github.com>
2021-04-17 18:04:28 -0700

    Update scraper.py
    worked on verifying urls we want to hit

--- scraper.py
+++ scraper.py
@@ -1,19 +1,41 @@
 import re
 from urllib.parse import urlparse
+import requests
+
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
     return [link for link in links if is_valid(link)]
 
+# Implementation required.
+
+
 def extract_next_links(url, resp):
-    # Implementation requred.
-    return list()
+    # create a list to return to scraper()
+    thing = []
+
+    # check if url is valid
+    if is_valid(url):
+        print("good")
+
+    return thing
+
 
 def is_valid(url):
     try:
+        # Parse a URL into 6 components:
+        # <scheme>://<netloc>/<path>;<params>?<query>#<fragment>
         parsed = urlparse(url)
         if parsed.scheme not in set(["http", "https"]):
             return False
+        # Checks the <netloc> part of the url to see if it's valid
+        return not re.match(
+            r".ics.uci.edu/" |
+            + r".cs.uci.edu/" |
+            + r".informatics.uci.edu/" |
+            + r".stat.uci.edu/" |
+            + r"today.uci.edu/department/information_computer_sciences/", parsed.netloc)
+        # Checks the <path> part of the URL to see if it's valid
         return not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
@@ -25,5 +47,5 @@ def is_valid(url):
             + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
 
     except TypeError:
-        print ("TypeError for ", parsed)
+        print("TypeError for ", parsed)
         raise

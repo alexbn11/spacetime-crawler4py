ee18f8fc06125478ac102660f92fec34563dd50d
Alex Ba Nguyen <alexbn@circinus-42.ics.uci.edu>
2021-04-24 02:43:06 -0700

    Changed Politeness, Regex Adjusted, Added Minor Logging

--- Delete.py
+++ Delete.py
@@ -1,16 +1,18 @@
 import re
 import requests
 import nltk
-from nltk.corpus import stopwords 
-from nltk.tokenize import word_tokenize 
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 from urllib.parse import urlparse
 from bs4 import BeautifulSoup
+from urllib.request import Request, urlopen
+import re
 """
 # Making a get request
 response = requests.get('http://www.ics.uci.edu/~sharad/personal-home-page')
-  
+
 # prinitng request text
-#print(response.text)
+# print(response.text)
 
 if response:
     print('Success!')
@@ -19,7 +21,7 @@ else:
 
  # http ://calendar.ics.uci.edu/calendar.php?type=month&calend
 """
-#<scheme>://<netloc>/<path>;<params>?<query>#<fragment>
+# <scheme>://<netloc>/<path>;<params>?<query>#<fragment>
 # If zero or more characters at the beginning of string match the regular expression pattern, return a corresponding match object.
 # Return None if the string does not match the pattern; note that this is different from a zero-length match.
 # Note that even in MULTILINE mode, re.match() will only match at the beginning of the string and not at the beginning of each line.
@@ -29,14 +31,75 @@ else:
 txt = "ngs.ics.uci.edu"
 txt = "www.ics.uci.edu"
 txt = "calendar.php?type=month&calend"
-parsed = urlparse('https://evoke.ics.uci.edu/values-in-design-fellows-honored-at-iconference-2013/?replytocom=43778#respond')
-print(parsed)
-print(parsed.netloc)
-print(parsed.path)
-print(parsed.query)
+parsed = urlparse('https://www.stat.uci.edu/news/page/2')
+ #   'https://evoke.ics.uci.edu/values-in-design-fellows-honored-at-iconference-2013/?replytocom=43778#respond')
+parsed2 = urlparse(
+    'http://www.example.com/shoes?sex=men&color=black&size=44&sale=no')
+parsed1 = urlparse('https://speedtest.ics.uci.edu/')
+# https://www.stat.uci.edu/news/page/2
+# print(parsed)
+# print(parsed.netloc)
+# print(parsed.path)
+# print(parsed.query)
+#print(parsed1.status_code)
+#print(parsed)
 
 
-if re.match(r".*\?", parsed.netloc):
-    print("question?")
+req = Request("https://www.stat.uci.edu//www.stat.uci.edu/news")
+html_page = urlopen(req)
+
+soup = BeautifulSoup(html_page, "lxml")
+
+links = []
+for link in soup.findAll('a'):
+    links.append(link.get('href'))
+
+print("Links:", links)
 
 
+'''if parsed2.query:
+    print("TRIGGERED")
+else:
+    print("PASSED")
+if re.match(r'.*\?.*|.*\/', parsed.query.lower()):
+            return False'''
+
+'''if parsed1.fragment:
+    print("TRIGGERED")
+else:
+    print("PASSED")
+'''
+if parsed.fragment:
+    print("FAILED:FRAG")
+
+
+if parsed.scheme not in set(["http", "https"]):
+    print("FAILED:SCHEME")
+
+if not re.match(
+    r".+\.ics\.uci\.edu"
+    + r"|.+\.cs\.uci\.edu"  # |in front helps sperate the searches
+    + r"|.+\.informatics\.uci\.edu"
+        + r"|.+\.stat\.uci\.edu", parsed.netloc):
+    print("FAILED:NETLOC")
+elif re.match(r"today\.uci\.edu/?", parsed.netloc) and re.match(r"department/information_computer_sciences/?", parsed.path.lower()):
+    print("PASSED:NETLOC")
+
+
+if re.match(r"(www\.)?calendar", parsed.netloc):
+    print("FAILED:NETLOC")
+
+
+if parsed.query:
+    print("FAILED:QUERY")
+
+if re.match(
+    r".*\.(css|js|bmp|gif|jpe?g|ico"
+    + r"|png|tiff?|mid|mp2|mp3|mp4"
+    + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
+    + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
+    + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
+    + r"|epub|dll|cnf|tgz|sha1"
+    + r"|thmx|mso|arff|rtf|jar|csv"
+    + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower()):
+    print("FAILED:PATH")
--- scraper.py
+++ scraper.py
@@ -12,10 +12,12 @@ def scraper(url, resp):
     links = []
 
     if resp.raw_response:
-        #print('Success!, 200 <= raw_response <= 400 ')
+        # print('Success!, 200 <= raw_response <= 400 ')
         links = extract_next_links(url, resp)
     else:
-        print('An error has occurred.')
+        print('Reject', "Status Code:", resp.status))
+        # print("Code:", resp.raw_response.status_code)Causes ERROR
+        
 
     return [link for link in links if is_valid(link)]
 
@@ -26,23 +28,24 @@ def extract_next_links(url, resp):
     # print("extracting...")
     # create a list to return to scraper()
     tingz = []
-
     resp.raw_response.encoding = 'utf-8'
     soup = BeautifulSoup(resp.raw_response.text, "lxml")
     linkers = []
+    # reterive all links from webpage
     for link in soup.findAll('a'):
         linkers.append(link.get('href'))
 
-    # check if url is valid
-    for url in linkers:
+    # check if link is valid and/or relative
+    for link in linkers:
+
         if link != None and re.match(r'\/.*', link):
-            relativeLink = link
+            reLink = link
             parsed = urlparse(url)
             link = str(parsed.scheme) + '://' + \
-                str(parsed.netloc) + str(link)
+                       str(parsed.netloc) + str(reLink)
 
-        if is_valid(url):
-            tingz.append(url)
+        if is_valid(link):
+            tingz.append(link)
 
     return tingz
 
@@ -60,29 +63,24 @@ def is_valid(url):
         if parsed.scheme not in set(["http", "https"]):
             return False
         # Checks the <netloc> part of the url to see if it's valid
-        #print("Checking <netloc>: {}".format(parsed.netloc))
+        # print("Checking <netloc>: {}".format(parsed.netloc))
         if not re.match(
             r".+\.ics\.uci\.edu"
             + r"|.+\.cs\.uci\.edu"  # |in front helps sperate the searches
             + r"|.+\.informatics\.uci\.edu"
-                + r"|.+\.stat\.uci\.edu", parsed.netloc):
+            + r"|.+\.stat\.uci\.edu", parsed.netloc):
             return False
         elif re.match(r"today\.uci\.edu/?", parsed.netloc) and re.match(r"department/information_computer_sciences/?", parsed.path.lower()):
             return True
 
-        #print("Checking <netloc>: {} for traps".format(parsed.netloc))
+        # print("Checking <netloc>: {} for traps".format(parsed.netloc))
         if re.match(r"(www\.)?calendar", parsed.netloc):
-            return False
-
-        # avoid Queries
-        # <scheme>://<netloc>/<path>;<params>?<query>#<fragment>
-        if re.search(r"replytocom=", parsed.query.lower()):
             return False
 
         # Checks the <path> part of the URL to see if it's valid
         # If <path> ends with this file extension   .\.
         # re.match finds a match which returns True, not makes it false
-        #print("Checking <path>:{}".format(parsed.path.lower()))
+        # print("Checking <path>:{}".format(parsed.path.lower()))
         if re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
@@ -92,6 +90,17 @@ def is_valid(url):
             + r"|epub|dll|cnf|tgz|sha1"
             + r"|thmx|mso|arff|rtf|jar|csv"
                 + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower()):
+            return False
+
+        # avoid Queries
+        # <scheme>://<netloc>/<path>;<params>?<query>#<fragment>
+        # if re.search(r"replytocom=", parsed.query.lower()):
+        #    return False
+        if parsed.query:
+            return False
+
+        # avoid Fragments
+        if parsed.fragment:
             return False
         return True
 
